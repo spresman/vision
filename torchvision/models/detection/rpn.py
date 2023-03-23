b73f340193df73b3c2052569d54a4cea3db64e1f
Sam Presman <sampresman@gmail.com>
2023-03-22 20:36:33 -0400

    Updated rpn compute_loss to return a vector instead of the summation

--- torchvision/models/detection/rpn.py
+++ torchvision/models/detection/rpn.py
@@ -325,7 +325,7 @@ class RegionProposalNetwork(torch.nn.Module):
             pred_bbox_deltas[sampled_pos_inds],
             regression_targets[sampled_pos_inds],
             beta=1 / 9,
-            reduction="sum",
+            reduction="none",
         ) / (sampled_inds.numel())
 
         objectness_loss = F.binary_cross_entropy_with_logits(objectness[sampled_inds], labels[sampled_inds])
